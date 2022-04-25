3089af495d9abdf9a93e4c0eff8d01bf34e205c6
luotn <luotn@hotmail.com>
2022-04-25 14:12:27 +0100

    Initial Commit

---
+++ Reality Parser/Reality_ParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reality_ParserApp.swift
+//  Reality Parser
+//
+//  Created by 罗天宁 on 25/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Reality_ParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
